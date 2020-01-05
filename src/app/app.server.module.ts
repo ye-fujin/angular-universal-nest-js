7eab5cd30ddc18e8581fe5c453168d415bf6ec00
YeFujin <evgeny.shelukhin@hotmail.com>
2020-01-06 00:13:17 +0300

    A: universal interceptor

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,9 +1,10 @@
+import { UniversalInterceptor } from './universal-interceptor';
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   imports: [
@@ -12,5 +13,10 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
     ModuleMapLoaderModule,
   ],
   bootstrap: [AppComponent],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: UniversalInterceptor,
+    multi: true
+  }]
 })
 export class AppServerModule {}
